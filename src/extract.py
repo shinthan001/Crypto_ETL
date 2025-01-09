23a1b25d97a6dc46e20cef988f56d71f7b3bffb6
Shin <shinthantaun99@gmail.com>
2025-01-09 17:55:53 +0800

    updated

---
+++ src/extract.py
@@ -0,0 +1,70 @@
+import os
+import requests
+import pandas as pd
+from io import StringIO 
+from dotenv import load_dotenv
+from requests.exceptions import RequestException
+
+# from requests.utils import DEFAULT_CA_BUNDLE_PATH
+# import certifi
+# print(certifi.where())
+
+def get_proxies():
+    http_proxy = os.getenv('HTTP_PROXY')
+    https_proxy = os.getenv('HTTPS_PROXY')
+
+    return {
+        "http" : http_proxy, 
+        "https" : https_proxy
+    }
+
+def get_ssl_cert():
+    return os.getenv('SSL_VERIFY')
+
+def request_api(url:str, headers:dict):
+    proxies = get_proxies()
+    ssl_cert = get_ssl_cert()
+
+    try:
+        response = requests.get(url,headers=headers, proxies=proxies, verify=ssl_cert)
+    except RequestException as err:
+        raise(err)
+    return response
+
+def to_dataframe(response:requests.models.Response):
+    df = pd.read_json(StringIO(response.text))
+                    #   ,orient='records', lines=True)
+    return df
+
+def to_json(df:pd.DataFrame, tgt_dir:str, fname:str):
+    file_path = f'{tgt_dir}/{fname}.json'
+    os.makedirs(f'{tgt_dir}', exist_ok=True)
+    try:
+        df.to_json(file_path)
+                #    ,orient='records', lines=True)
+    except FileNotFoundError as err:
+        raise(err)
+
+    print(f"Successfully saved to {file_path}.\n")
+
+def extract_api(url:str, tgt_dir:str, fname:str, headers:dict=None):
+    response = request_api(url, headers)
+    df = to_dataframe(response)
+    to_json(df,tgt_dir,fname)
+
+def extract_data():
+    coinslist_api = os.getenv('COINSLIST_EP')
+    coingecko_apikey = os.getenv('APIKEY_COINGECKO')
+    tgt_dir = os.getenv('EXTRACTED_DIR')
+    
+    coinhecko_headers = {
+        'User-Agent': 'Chrome',
+        "accept" : "application/json",
+        "x-cg-demo-api-key": coingecko_apikey
+    }
+
+    fname = 'coinslist'
+    extract_api(coinslist_api, tgt_dir, fname, coinhecko_headers)
+
+load_dotenv('.env')
+extract_data()
